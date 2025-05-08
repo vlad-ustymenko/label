a71558d82abc7c92a9dc1eca47e49937ad909697
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-05-08 19:41:17 +0300

    Create projectsDTO

---
+++ DTO/projects.js
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    id: 0,
+    title: "Project 1",
+    description: "Project 1 description",
+    url: "/project.png",
+    yaer: "13.02.22",
+  },
+  {
+    id: 1,
+    title: "Project 2",
+    description: "Project 2 description",
+    url: "/project.png",
+    yaer: "01.20.23",
+  },
+  {
+    id: 2,
+    title: "Project 3",
+    description: "Project 3 description",
+    url: "/project.png",
+    yaer: "21.07.25",
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    description: "Project 3 description",
+    url: "/project.png",
+    yaer: "21.07.25",
+  },
+];
