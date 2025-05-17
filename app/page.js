f42298e1d36299e8e67b6e4a701908d40c24deea
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-05-17 20:16:51 +0300

    Edit MainPage

--- app/page.js
+++ app/page.js
@@ -1,34 +1,15 @@
-"use client";
-import MainScreen from "@/components/MainScreen/MainScreen";
+import MainScreen from "../components/MainScreen/MainScreen";
 import About from "@/components/About/About";
-import BlurAnimation from "@/components/BlurAnimation/BlurAnimation";
-import styles from "./page.module.css";
 import Roadmap from "@/components/Roadmap/Roadmap";
-import { useEffect } from "react";
 
 export default function Home() {
-  // useEffect(() => {
-  //   (async () => {
-  //     const LocomotiveScroll = (await import("locomotive-scroll")).default;
-
-  //     new LocomotiveScroll({
-  //       el: document.querySelector("[data-scroll-container]"),
-  //       smooth: true, // обов'язково
-  //       lerp: 0.05, // 0.01 - дуже плавно, 0.1 - швидше
-  //       multiplier: 0.5, // швидкість прокрутки (1 = нормальна, <1 = повільніше)
-  //       touchMultiplier: 2, // чутливість скролу на мобілках
-  //       resetNativeScroll: true,
-  //     });
-  //   })();
-  // }, []);
-
   return (
-    <main data-scroll-container>
+    <main>
       <MainScreen />
-      {/* <BlurAnimation /> */}
       <About />
       <Roadmap />
-      <div style={{ height: "1000vh" }}></div>
+
+      <div style={{ height: "300vh" }} />
     </main>
   );
 }
